3eaf90e50212b025ed994c71f74875ff9bab5795
Basil Kotov <basilkot@gmail.com>
2024-02-21 01:26:29 -0800

    VCST-334: fixes (#3)

--- src/VirtoCommerce.Skyflow.Data/Services/SkyflowClient.cs
+++ src/VirtoCommerce.Skyflow.Data/Services/SkyflowClient.cs
@@ -1,18 +1,13 @@
 using System;
-using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
-using JWT;
 using JWT.Algorithms;
 using JWT.Builder;
-using JWT.Serializers;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using VirtoCommerce.Platform.Core.Exceptions;
 using VirtoCommerce.Skyflow.Core.Models;
@@ -141,47 +136,44 @@ namespace VirtoCommerce.Skyflow.Data.Services
             return result;
         }
 
-#if DEBUG
+        //private string Decode(SkyflowSdkOptions options, string jwt)
+        //{
+        //    var certificate = CreateCertificate(options);
+        //    IJsonSerializer serializer = new JsonNetSerializer();
+        //    IDateTimeProvider provider = new UtcDateTimeProvider();
+        //    IJwtValidator validator = new JwtValidator(serializer, provider);
+        //    IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+        //    IJwtAlgorithm algorithm = new RS256Algorithm(certificate);
+        //    IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
 
-        private string Decode(SkyflowSdkOptions options, string jwt)
-        {
-            var certificate = CreateCertificate(options);
-            IJsonSerializer serializer = new JsonNetSerializer();
-            IDateTimeProvider provider = new UtcDateTimeProvider();
-            IJwtValidator validator = new JwtValidator(serializer, provider);
-            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
-            IJwtAlgorithm algorithm = new RS256Algorithm(certificate);
-            IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+        //    var json = decoder.Decode(jwt);
+        //    return json;
+        //}
 
-            var json = decoder.Decode(jwt);
-            return json;
-        }
+        //private string GenerateTokenStandard(SkyflowSdkOptions options)
+        //{
+        //    // way to get token using standard library
 
-        private string GenerateTokenStandard(SkyflowSdkOptions options)
-        {
-            // way to get token using standard library
+        //    var privateKey = GetPrivateKey(options);
+        //    var rsa = RSA.Create();
+        //    rsa.ImportPkcs8PrivateKey(Convert.FromBase64String(privateKey), out _);
 
-            var privateKey = GetPrivateKey(options);
-            var rsa = RSA.Create();
-            rsa.ImportPkcs8PrivateKey(Convert.FromBase64String(privateKey), out _);
+        //    var signingCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
 
-            var signingCredentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
+        //    var claims = new Claim[]
+        //    {
+        //        new(JwtRegisteredClaimNames.Iss, options.ClientId),
+        //        new("key", options.KeyId),
+        //        new(JwtRegisteredClaimNames.Aud, _options.TokenUri),
+        //        new(JwtRegisteredClaimNames.Exp, DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds().ToString()),
+        //        new(JwtRegisteredClaimNames.Sub, options.ClientId)
+        //    };
 
-            var claims = new Claim[]
-            {
-                new(JwtRegisteredClaimNames.Iss, options.ClientId),
-                new("key", options.KeyId),
-                new(JwtRegisteredClaimNames.Aud, _options.TokenUri),
-                new(JwtRegisteredClaimNames.Exp, DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds().ToString()),
-                new(JwtRegisteredClaimNames.Sub, options.ClientId)
-            };
+        //    var token = new JwtSecurityToken(claims: claims, signingCredentials: signingCredentials);
 
-            var token = new JwtSecurityToken(claims: claims, signingCredentials: signingCredentials);
-
-            var handler = new JwtSecurityTokenHandler();
-            var tokenString = handler.WriteToken(token);
-            return tokenString;
-        }
-#endif
+        //    var handler = new JwtSecurityTokenHandler();
+        //    var tokenString = handler.WriteToken(token);
+        //    return tokenString;
+        //}
     }
 }
