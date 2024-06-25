8ca2c7b2f29689889a59a5a2c11bd970ede57dd7
Tatarincev Eugeney <et@virtoway.com>
2024-06-25 11:09:31 +0200

    VCST-917: Rework skyflow implementation (#7)
    Co-authored-by: Basil Kotov <basilkot@gmail.com>

--- src/VirtoCommerce.Skyflow.Core/Models/SkyflowStoreConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace VirtoCommerce.Skyflow.Core.Models;
-
-public class SkyflowStoreConfig
-{
-    public string VaultId { get; set; }
-    public string VaultUrl { get; set; }
-    public string TableName { get; set; }
-}
--- src/VirtoCommerce.Skyflow.Data/Providers/SkyflowPaymentMethod.cs
+++ src/VirtoCommerce.Skyflow.Data/Providers/SkyflowPaymentMethod.cs
@@ -2,24 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using VirtoCommerce.OrdersModule.Core.Model;
 using VirtoCommerce.PaymentModule.Core.Model;
+using VirtoCommerce.PaymentModule.Core.Services;
 using VirtoCommerce.PaymentModule.Model.Requests;
-using VirtoCommerce.Platform.Core.Settings;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Skyflow.Core;
+using VirtoCommerce.Skyflow.Core.Models;
 using VirtoCommerce.Skyflow.Core.Services;
-using VirtoCommerce.Skyflow.Data.Extensions;
 
 namespace VirtoCommerce.Skyflow.Data.Providers
 {
-    public class SkyflowPaymentMethod(ISkyflowClient skyflowClient, IPaymentClientFactory paymentClientFactory) : PaymentMethod(nameof(SkyflowPaymentMethod))
+    public class SkyflowPaymentMethod : PaymentMethod
     {
+        private readonly SkyflowOptions _options;
+        private readonly ISkyflowClient _skyFlowClient;
+        private readonly IPaymentMethodsRegistrar _paymentMethodsRegistrar;
+        public SkyflowPaymentMethod(
+            ISkyflowClient skyflowClient,
+            IPaymentMethodsRegistrar paymentMethodsRegistrar,
+            IOptions<SkyflowOptions> options)
+            : base(nameof(SkyflowPaymentMethod))
+        {
+            _skyFlowClient = skyflowClient;
+            _options = options.Value;
+            _paymentMethodsRegistrar = paymentMethodsRegistrar;
+        }
+
         public override ProcessPaymentRequestResult ProcessPayment(ProcessPaymentRequest request)
         {
-            var tokenResponse = skyflowClient.GetBearerToken().Result;
-            var vaultId = Settings.GetValue<string>(ModuleConstants.Settings.General.VaultId);
-            var vaultUrl = Settings.GetValue<string>(ModuleConstants.Settings.General.VaultUrl);
-            var tableName = Settings.GetValue<string>(ModuleConstants.Settings.General.TableName);
+            var tokenResponse = _skyFlowClient.GetBearerToken(_options.PaymentFormAccount).GetAwaiter().GetResult();
 
             var result = new ProcessPaymentRequestResult
             {
@@ -28,12 +43,11 @@ namespace VirtoCommerce.Skyflow.Data.Providers
                 PublicParameters = new Dictionary<string, string>
                 {
                     {"accessToken", tokenResponse.AccessToken},
-                    {"vaultID", vaultId},
-                    {"vaultURL", vaultUrl},
-                    {"tableName", tableName}
+                    {"vaultID", _options.VaultId},
+                    {"vaultURL", $"{_options.VaultUri}"},
+                    {"tableName", _options.TableName}
                 }
             };
-
             var payment = (PaymentIn)request.Payment;
             payment.PaymentStatus = PaymentStatus.Pending;
             payment.Status = payment.PaymentStatus.ToString();
@@ -43,39 +57,45 @@ namespace VirtoCommerce.Skyflow.Data.Providers
 
         public override PostProcessPaymentRequestResult PostProcessPayment(PostProcessPaymentRequest request)
         {
-            var paymentClient = paymentClientFactory.GetPaymentClient(request);
-            var connectionName = paymentClientFactory.GetConnectionName(request);
+            return PostProcessPaymentAsync(request).GetAwaiter().GetResult();
+        }
 
-            if (paymentClient.RequiredParameters.FirstOrDefault(x => request.Parameters.AllKeys.All(k => k != x)) != null)
+        protected virtual async Task<PaymentMethod> GetTargetPaymentMethod(PostProcessPaymentRequest request)
+        {
+            var paymentMethod = (await _paymentMethodsRegistrar.GetRegisteredPaymentMethods()).FirstOrDefault(x => x.TypeName.EqualsInvariant(_options.TargetPaymentMethod));
+            if (paymentMethod == null)
             {
-                var skyflowId = request.Parameters["skyflow_id"];
-                if (string.IsNullOrEmpty(skyflowId))
-                {
-                    throw new InvalidOperationException("Skyflow ID is required");
-                }
+                throw new OperationCanceledException("Payment method not found. Please check the Payments:Skyflow:TargetPaymentMethod setting");
+            }
+            return paymentMethod;
+        }
 
-                var config = Settings.GetSkyflowStoreConfig();
-                var order = (CustomerOrder)request.Order;
-                var userId = order.CustomerId;
+        private async Task<PostProcessPaymentRequestResult> PostProcessPaymentAsync(PostProcessPaymentRequest request)
+        {
+            var paymentMethod = await GetTargetPaymentMethod(request);
 
-                var tokens = skyflowClient.GetCardTokens(config, skyflowId).GetAwaiter().GetResult();
-
-                if (tokens["user_id"] != userId)
-                {
-                    throw new InvalidOperationException("Skyflow ID does not belong to the user");
-                }
-
-                foreach (var key in tokens.Keys)
-                {
-                    request.Parameters[key] = tokens[key];
-                }
+            var skyflowId = request.Parameters["skyflow_id"];
+            if (string.IsNullOrEmpty(skyflowId))
+            {
+                throw new InvalidOperationException("Skyflow ID is required");
             }
 
-            using var requestMessage = paymentClient.CreateConnectionRequest(request);
-            using var responseMessage = skyflowClient.InvokeConnection(connectionName, requestMessage)
-                .GetAwaiter().GetResult();
+            var order = (CustomerOrder)request.Order;
 
-            var result = paymentClient.CreatePostProcessPaymentResponse(request, responseMessage);
+            var skyFlowCard = await _skyFlowClient.GetCard(skyflowId);
+            if (skyFlowCard == null)
+            {
+                throw new OperationCanceledException("Skyflow card record not found");
+            }
+            if (!string.IsNullOrEmpty(skyFlowCard.UserId) && skyFlowCard.UserId != order.CustomerId)
+            {
+                throw new UnauthorizedAccessException("Payment cannot be processed using a card registered to another user");
+            }
+
+            request.Parameters["CreditCard"] = JsonConvert.SerializeObject(skyFlowCard);
+            request.Parameters["ProxyHttpClientName"] = ModuleConstants.SkyflowHttpClientName;
+            request.Parameters["ProxyEndpointUrl"] = new Uri($"{_options.GatewayUri}/v1/gateway/outboundRoutes/{_options.TargetConnectionRoute}").ToString();
+            var result = paymentMethod.PostProcessPayment(request);
             return result;
         }
 
