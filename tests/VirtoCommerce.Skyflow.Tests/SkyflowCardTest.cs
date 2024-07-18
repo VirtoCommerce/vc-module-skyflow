1fa5f7eff265317a9db758eae1a6188c086fe302
Basil Kotov <basilkot@gmail.com>
2024-07-18 10:14:10 +0100

    VCST-1287: Add Active property to card model (#9)
    feat: add Inactive property to the skyflowCard model and business logic for calculation of this property value
    feat: added unit-tests

---
+++ tests/VirtoCommerce.Skyflow.Tests/SkyflowCardTest.cs
@@ -0,0 +1,43 @@
+using System;
+using VirtoCommerce.Skyflow.Core.Models;
+using Xunit;
+
+namespace VirtoCommerce.Skyflow.Tests;
+
+[Trait("Category", "Unit")]
+public class SkyflowCardTest
+{
+    [Theory]
+    [InlineData("10", "20", false)]
+    [InlineData("01", "38", true)]
+    [InlineData("02", "98", true)]
+    public void MonthYearTest(string month, string year, bool isActive)
+    {
+        var card = new SkyflowCard { ExpiryMonth = month, ExpiryYear = year, };
+
+        Assert.Equal(isActive, card.Active);
+    }
+
+    [Theory]
+    [InlineData("10/20", false)]
+    [InlineData("01/2038", true)]
+    [InlineData("02/98", true)]
+    public void ExpireDateTest(string expireDate, bool isActive)
+    {
+        var card = new SkyflowCard { CardExpiration = expireDate };
+
+        Assert.Equal(isActive, card.Active);
+    }
+
+    [Fact]
+    public void CurrentMonthTests()
+    {
+        var month = DateTime.UtcNow.Month.ToString("00");
+        var year = DateTime.UtcNow.Year.ToString("00");
+        var card = new SkyflowCard { ExpiryMonth = month, ExpiryYear = year, };
+
+        Assert.True(card.Active);
+    }
+
+
+}
