c860adb4f453adb5ae736624d6979605b3671216
Oleg Zhuk <zhukoo@gmail.com>
2024-07-25 11:28:22 +0200

    fix compilation error

--- src/VirtoCommerce.Skyflow.XApi/Commands/DeleteSkyflowCardCommandBuilder.cs
+++ src/VirtoCommerce.Skyflow.XApi/Commands/DeleteSkyflowCardCommandBuilder.cs
@@ -3,34 +3,19 @@ using GraphQL.Types;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using VirtoCommerce.Xapi.Core.BaseQueries;
+using VirtoCommerce.Xapi.Core.Extensions;
 
 namespace VirtoCommerce.Skyflow.XApi.Commands;
 
 public class DeleteSkyflowCardCommandBuilder(IMediator mediator,
         IAuthorizationService authorizationService
-        //ISkyflowClient skyflowClient,
-        //ISettingsManager settingsManager,
-        //IPaymentMethodsSearchService paymentMethods
         ) : CommandBuilder<DeleteSkyflowCardCommand, bool, DeleteSkyflowCardCommandType, BooleanGraphType>(mediator, authorizationService)
 {
     protected override string Name => "DeleteSkyflowCard";
 
-    protected override async Task BeforeMediatorSend(IResolveFieldContext<object> context, DeleteSkyflowCardCommand request)
+    protected override Task BeforeMediatorSend(IResolveFieldContext<object> context, DeleteSkyflowCardCommand request)
     {
-        // Skyflow doesn't return user_id in the response, so we can't check if the user is the owner of the card
-        //var userId = context.GetCurrentUserId();
-
-        //var config = await paymentMethods.GetSettingsAsync(settingsManager, request.StoreId);
-
-        //var card = await skyflowClient.GetCardTokens(config, request.SkyflowId);
-
-        //if (card["user_id"] != userId)
-        //{
-        //    throw context.IsAuthenticated()
-        //        ? AuthorizationError.Forbidden()
-        //        : AuthorizationError.AnonymousAccessDenied();
-        //}
-        await base.BeforeMediatorSend(context, request);
+        return base.BeforeMediatorSend(context, request);
     }
 
     protected override DeleteSkyflowCardCommand GetRequest(IResolveFieldContext<object> context)
