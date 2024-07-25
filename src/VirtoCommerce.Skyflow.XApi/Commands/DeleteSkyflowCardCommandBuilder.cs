edad21806f5813fdeee4775552684189b5870ab8
Tatarincev Eugeney <et@virtoway.com>
2024-07-25 11:38:16 +0200

    VCST-1555: Minor improvements and bugfixes (#10)
    Co-authored-by: Oleg Zhuk <zhukoo@gmail.com>

--- src/VirtoCommerce.Skyflow.XApi/Commands/DeleteSkyflowCardCommandBuilder.cs
+++ src/VirtoCommerce.Skyflow.XApi/Commands/DeleteSkyflowCardCommandBuilder.cs
@@ -3,33 +3,25 @@ using GraphQL.Types;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using VirtoCommerce.ExperienceApiModule.Core.BaseQueries;
+using VirtoCommerce.ExperienceApiModule.Core.Extensions;
 
 namespace VirtoCommerce.Skyflow.XApi.Commands;
 
 public class DeleteSkyflowCardCommandBuilder(IMediator mediator,
         IAuthorizationService authorizationService
-        //ISkyflowClient skyflowClient,
-        //ISettingsManager settingsManager,
-        //IPaymentMethodsSearchService paymentMethods
         ) : CommandBuilder<DeleteSkyflowCardCommand, bool, DeleteSkyflowCardCommandType, BooleanGraphType>(mediator, authorizationService)
 {
     protected override string Name => "DeleteSkyflowCard";
 
-    protected override async Task BeforeMediatorSend(IResolveFieldContext<object> context, DeleteSkyflowCardCommand request)
+    protected override Task BeforeMediatorSend(IResolveFieldContext<object> context, DeleteSkyflowCardCommand request)
     {
-        // Skyflow doesn't return user_id in the response, so we can't check if the user is the owner of the card
-        //var userId = context.GetCurrentUserId();
+        return base.BeforeMediatorSend(context, request);
+    }
 
-        //var config = await paymentMethods.GetSettingsAsync(settingsManager, request.StoreId);
-
-        //var card = await skyflowClient.GetCardTokens(config, request.SkyflowId);
-
-        //if (card["user_id"] != userId)
-        //{
-        //    throw context.IsAuthenticated()
-        //        ? AuthorizationError.Forbidden()
-        //        : AuthorizationError.AnonymousAccessDenied();
-        //}
-        await base.BeforeMediatorSend(context, request);
+    protected override DeleteSkyflowCardCommand GetRequest(IResolveFieldContext<object> context)
+    {
+        var result = base.GetRequest(context);
+        result.UserId = context.GetCurrentUserId();
+        return result;
     }
 }
