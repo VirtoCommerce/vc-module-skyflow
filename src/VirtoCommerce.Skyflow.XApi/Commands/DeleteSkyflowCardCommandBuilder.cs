45ad9d09f73d764961f1e972c5ca3145b88a5ca8
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2026-07-27 17:19:29 +0300

    VCST-5468: Resolve mediators per request (#24)

--- src/VirtoCommerce.Skyflow.XApi/Commands/DeleteSkyflowCardCommandBuilder.cs
+++ src/VirtoCommerce.Skyflow.XApi/Commands/DeleteSkyflowCardCommandBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using GraphQL;
 using GraphQL.Types;
 using MediatR;
@@ -7,10 +8,15 @@ using VirtoCommerce.Xapi.Core.Extensions;
 
 namespace VirtoCommerce.Skyflow.XApi.Commands;
 
-public class DeleteSkyflowCardCommandBuilder(IMediator mediator,
-        IAuthorizationService authorizationService
-        ) : CommandBuilder<DeleteSkyflowCardCommand, bool, DeleteSkyflowCardCommandType, BooleanGraphType>(mediator, authorizationService)
+public class DeleteSkyflowCardCommandBuilder(IAuthorizationService authorizationService)
+    : CommandBuilder<DeleteSkyflowCardCommand, bool, DeleteSkyflowCardCommandType, BooleanGraphType>(authorizationService)
 {
+    [Obsolete("Use the constructor without IMediator. The mediator is resolved from context.RequestServices per request.", DiagnosticId = "VC0015", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions")]
+    public DeleteSkyflowCardCommandBuilder(IMediator mediator, IAuthorizationService authorizationService)
+        : this(authorizationService)
+    {
+    }
+
     protected override string Name => "DeleteSkyflowCard";
 
     protected override Task BeforeMediatorSend(IResolveFieldContext<object> context, DeleteSkyflowCardCommand request)
@@ -22,6 +28,7 @@ public class DeleteSkyflowCardCommandBuilder(IMediator mediator,
     {
         var result = base.GetRequest(context);
         result.UserId = context.GetCurrentUserId();
+
         return result;
     }
 }
